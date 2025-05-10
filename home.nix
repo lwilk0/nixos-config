59591543082365438ba21fb450dd5d2d5e6fbc80
lwilk0 <wilkinsonluke@proton.me>
2025-05-10 18:31:43 +0100

    Update to config

--- home.nix
+++ home.nix
@@ -112,15 +112,15 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/.zshrc;
-    ".nix-channels".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/.nix-channels;
-    ".config/fastfetch/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/fastfetch/config.jsonc;
-    ".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/hypr/hyprland.conf;
-    ".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/kitty/kitty.conf;
-    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/nvim;
-    ".config/rofi".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/rofi;
-    ".config/Thunar".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/Thunar;
-    ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/waybar;
+    ".zshrc".source = ./home/.zshrc;
+    #".nix-channels".source = config.lib.file.mkOutOfStoreSymlink /home/wilko/.dotfiles/home/.nix-channels;
+    #".config/fastfetch/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/fastfetch/config.jsonc;
+    #".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/hypr/hyprland.conf;
+    #".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/kitty/kitty.conf;
+    #".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/nvim;
+    #".config/rofi".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/rofi;
+    #".config/Thunar".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/Thunar;
+    #".config/waybar".source = config.lib.file.mkOutOfStoreSymlink ./.dotfiles/home/config/waybar;
   };
 
   # Home Manager can also manage your environment variables through
