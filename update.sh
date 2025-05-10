2a2b4c8d317e5f328a7907c4a1ffb5aa38587584
lwilk0 <wilkinsonluke@proton.me>
2025-05-10 18:49:40 +0100

    Update to config

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ sudo nix flake update &&
 home-manager switch --flake . -b backup &&
 sudo nixos-rebuild switch --flake . --impure &&
 git commit -m "Update to config" &&
-git push origin master &&
+git push origin master
